53c5765c8ec0593e1c46a6fcb259354dd8d0e021
mrgick <45306973+mrgick@users.noreply.github.com>
2022-01-30 15:08:51 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,91 @@
+
+async function main(find_name, group_name) {
+    if (find_name) {
+
+        //Generate groups_found
+        let groups = await get_json("https://api-rasp-pskgu.herokuapp.com/groups?list_of_names=true")
+        let list_names = groups.groups_list
+        groups_found = []
+        for (let i = 0; i < list_names.length; i++) {
+            if (list_names[i].toLowerCase().indexOf(find_name.toLowerCase()) != -1) {
+                if (groups_found.length > 5) {
+                    break
+                }
+                groups_found.push(list_names[i])
+            }
+        }
+
+        groups_found.sort()
+        //console.log(groups_found);
+
+        //Generate groups list
+        let ul = document.createElement('ul');
+        ul.setAttribute('id', 'Groups');
+        document.getElementById('Groups_List').appendChild(ul);
+        groups_found.forEach(renderGroupList);
+
+        function renderGroupList(element, index, arr) {
+            let li = document.createElement('li');
+            li.setAttribute('class', 'item');
+            let a = document.createElement('a');
+            a.setAttribute('href', '?group_name=' + element)
+            li.appendChild(a);
+            ul.appendChild(li);
+            a.innerHTML = a.innerHTML + element;
+
+        }
+        return
+    } else if (group_name) {
+        let group = await get_json("https://api-rasp-pskgu.herokuapp.com/groups?name=" + group_name)
+        //console.log(group);
+
+        let rasp = document.getElementById('Group_Rasp')
+
+        //add name
+        let name = document.createElement('h1');
+        name.innerHTML = name.innerHTML + group.name;
+        rasp.appendChild(name);
+
+        let days_length = Object.keys(group.days).length
+        if (days_length == 0) {
+            let empty = document.createElement('h2');
+            empty.innerHTML = empty.innerHTML + 'Расписание пустое';
+            rasp.appendChild(empty);
+            return
+        }
+
+        // generate tables
+        let first_date = Object.keys(group.days)[0]
+        let last_date = new Date(Object.keys(group.days)[days_length - 1])
+        let day = get_monday(first_date)
+
+        while (new Date(day) <= last_date) {
+            let table = document.createElement('table');
+            let tbody = document.createElement('tbody');
+
+            generate_top(tbody)
+            for (let i = 0; i < 7; i++) {
+                let day_date = get_next_day(day, i)
+                gen_row_data(tbody, day_date, group.days[day_date])
+            }
+
+            table.appendChild(tbody);
+            rasp.appendChild(table);
+            let div = document.createElement('br');
+            rasp.appendChild(div);
+
+
+            //console.log(day);
+            day = get_next_day(day, 7)
+        }
+    }
+}
+
+
+
+window.onload = async function () {
+    const params = new URLSearchParams(window.location.search)
+    let find_name = params.get("find_group_name")
+    let group_name = params.get("group_name")
+    await main(find_name, group_name)
+}
---
+++ js/utils.js
@@ -0,0 +1,90 @@
+// Get data from api (make request)
+async function get_json(url) {
+    let response = await fetch(url);
+    let data = await response.json();
+    return data;
+}
+
+//--------------------------
+//Working with time
+function date_to_str(date) {
+    return date.toISOString().split('T')[0];
+}
+
+function get_monday(date) {
+    date = new Date(date);
+    let day = date.getDay();
+    let diff = date.getDate() - day + (day == 0 ? -6 : 1);
+    date = new Date(date.setDate(diff))
+    return date_to_str(date);
+}
+
+function get_next_day(date, n = 1) {
+    date = new Date(date);
+    let diff = date.getDate() + n;
+    date = new Date(date.setDate(diff))
+    return date_to_str(date);
+}
+//--------------------------
+
+//--------------------------
+//Working with tables
+
+function add_td(name, tr) {
+    let td = document.createElement('td');
+    td.innerHTML += name
+    tr.appendChild(td)
+}
+
+function gen_row_data(table, day, day_info) {
+    let tr = document.createElement('tr');
+    add_td(day, tr)
+    if (day_info) {
+        for (let i = 1; i <= 7; i++) {
+            if (day_info[i]) {
+                add_td(day_info[i], tr)
+            }
+            else {
+                add_td('', tr)
+            }
+        }
+        console.log(day_info);
+    }
+    else {
+        for (let i = 0; i < 7; i++) {
+            add_td('', tr)
+        }
+    }
+    //console.log(day);
+    table.appendChild(tr)
+}
+
+// Генерация шапки таблицы
+function generate_top(table) {
+    // Генерация номеров пар
+    function gen_num(table) {
+        let tr = document.createElement('tr');
+        add_td('Пары', tr)
+        for (let i = 1; i < 8; i++) {
+            add_td(i + '-я', tr)
+        }
+        table.appendChild(tr)
+    }
+    // Генерация времени пар
+    function gen_time(table) {
+        let tr = document.createElement('tr');
+        add_td('Время', tr)
+        add_td('08:30-10:00', tr)
+        add_td('10:15-11:45', tr)
+        add_td('12:30-14:00', tr)
+        add_td('14:15-15:45', tr)
+        add_td('16:00-17:30', tr)
+        add_td('18:00-19:30', tr)
+        add_td('19:40-21:10', tr)
+        table.appendChild(tr)
+    }
+
+    gen_num(table)
+    gen_time(table)
+}
+//--------------------------
