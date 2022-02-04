78f8bbbb1ee051f7a24c23144aa656a05f9a0832
mrgick <45306973+mrgick@users.noreply.github.com>
2022-02-04 17:19:34 +0300

    add more api methods

--- js/api.js
+++ js/api.js
@@ -8,13 +8,20 @@ async function get_json(url) {
     return data;
 }
 
-async function get_list_groups(type='list') {
-    res = await get_json(API_URL + 'groups?list_of_names='+type)
-    return res['list_of_names']
+async function get_list_groups(type = 'list') {
+    if (type == 'list' || type == 'structure') {
+        res = await get_json(API_URL + 'groups?list_of_names=' + type)
+        return res['list_of_names']
+    }
 }
 
 async function get_group_info(name) {
     res = await get_json(API_URL + "groups?name=" + name)
+    return res
+}
+
+async function get_all_groups_info() {
+    res = await get_group_info("all")
     return res
 }
 
