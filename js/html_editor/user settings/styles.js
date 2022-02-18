43f7f077a27838fdca1cc92f2f66c40c710ed812
Maxim Makarov <74417675+WhiteRain7@users.noreply.github.com>
2022-02-18 22:28:20 +0300

    global styles renew

--- js/html_editor/user settings/styles.js
+++ js/html_editor/user settings/styles.js
@@ -4,20 +4,25 @@ function create_class (class_name, ...style_content) {
 	let style = document.createElement('style')
 	style.setAttribute('type', 'text/css')
 	style.setAttribute('id', 'style_'+class_name)
-	if (style_content[0][0] == '{') style.innerHTML = `.${class_name} ${style_content[0]}`
-	else style.innerHTML = `.${class_name} { ${style_content.join('; ')}; }`
+	if (style_content.length > 0) {
+		if (style_content[0][0] == '{') style.innerHTML = `.${class_name} ${style_content[0]}`
+		else style.innerHTML = `.${class_name} { ${style_content.join('; ')}; }`
+	}
+	else style.innerHTML = `.${class_name} { }`
 	document.getElementsByTagName('head')[0].appendChild(style)
 }
 
 function generate_css_classes () {
+	let style_content = []
+	for (attrib in base_styles[MODE]) style_content.push(attrib + ': ' + base_styles[MODE][attrib])
+	create_class('base', ...style_content)
+
 	for (class_text in used_class_names) {
 		for (subclass_text in used_class_names[class_text]) {
-			let style_content = []
-			for (attrib in base_styles[MODE]) style_content.push(attrib + ': ' + base_styles[MODE][attrib])
-			create_class(used_class_names[class_text][subclass_text],
-						 ...style_content)
+			create_class(used_class_names[class_text][subclass_text])
 		}
 	}
+
 	for (cl_name in base_text_styles) {
 		for (scl_name in base_text_styles[cl_name]) {
 			for (attrib in base_text_styles[cl_name][scl_name]) {
@@ -25,18 +30,19 @@ function generate_css_classes () {
 			}
 		}
 	}
+
 	load_settings()
 }
 
 const base_styles = {'light':{
-							  'background-color':'#ffffff',
+							  'background-color':'transparent',
 							  'color'           :'#000000',
 							  'border'          :'0px solid #000000',
 							  'font-style'      :'normal',   // normal | italic
 					  		  'font-weight'     :'normal',   // normal | bold
 							  'text-decoration' :'none'},    // none | line-through | underline
 					 'dark': {
-							  'background-color':'#000000', // 333366 ?
+							  'background-color':'transparent', // 333366 ?
 							  'color'           :'#ffffff',
 							  'border'          :'0px solid #000000',
 							  'font-style'      :'normal',   // normal | italic
@@ -58,14 +64,14 @@ for (base_style in base_styles) {
 }
 
 const base_text_styles = {'lesson_type': {
-					'test'      : {'background-color': '#FFCCFF', 'border':'0px solid #CCCCCC'},
-					'test_2'    : {'background-color': '#FFCCFF', 'border':'0px solid #CCCCCC'},
-					'cons'      : {'background-color': '#FFCC99', 'border':'0px solid #CCCCCC'},
-					'exam'      : {'background-color': '#FFCCCC', 'border':'0px solid #CCCCCC'},
-					'practice'  : {'background-color': '#CCCCFF', 'border':'0px solid #CCCCCC'},
-					'lab'       : {'background-color': '#CCFFFF', 'border':'0px solid #CCCCCC'},
-					'lecture'   : {'background-color': '#CCFFCC', 'border':'0px solid #CCCCCC'},
-					'volkswagen': {'background-color': '#99CC99', 'border':'0px solid #CCCCCC'},
+					'test'      : {'background-color': '#FFCCFF', 'border':'0px solid #CCCCCC', 'color': '#000000'},
+					'test_2'    : {'background-color': '#FFCCFF', 'border':'0px solid #CCCCCC', 'color': '#000000'},
+					'cons'      : {'background-color': '#FFCC99', 'border':'0px solid #CCCCCC', 'color': '#000000'},
+					'exam'      : {'background-color': '#FFCCCC', 'border':'0px solid #CCCCCC', 'color': '#000000'},
+					'practice'  : {'background-color': '#CCCCFF', 'border':'0px solid #CCCCCC', 'color': '#000000'},
+					'lab'       : {'background-color': '#CCFFFF', 'border':'0px solid #CCCCCC', 'color': '#000000'},
+					'lecture'   : {'background-color': '#CCFFCC', 'border':'0px solid #CCCCCC', 'color': '#000000'},
+					'volkswagen': {'background-color': '#99CC99', 'border':'0px solid #CCCCCC', 'color': '#000000'},
 }}
 
 function check_is_base (style_tag, mode = null) {
@@ -77,20 +83,15 @@ function check_is_base (style_tag, mode = null) {
 }
 
 function update_base_styles (was, became) {
-	for (class_text in used_class_names) {
-		for (subclass_text in used_class_names[class_text]) {
-			let full_class_name = used_class_names[class_text][subclass_text]
-			let style_tag = document.getElementById('style_'+full_class_name)
+	let style_tag = document.getElementById('style_base')
 
-			if (check_is_base(style_tag, was)) {
-				set_css_attribute('background-color', base_styles[became]['background-color'], style_tag)
-				set_css_attribute('color', base_styles[became]['color'], style_tag)
-				set_css_attribute('border', base_styles[became]['border'], style_tag)
-				set_css_attribute('font-style', base_styles[became]['font-style'], style_tag)
-				set_css_attribute('font-weight', base_styles[became]['font-weight'], style_tag)
-				set_css_attribute('text-decoration', base_styles[became]['text-decoration'], style_tag)
-			}
-		}
+	if (check_is_base(style_tag, was)) {
+		set_css_attribute('background-color', base_styles[became]['background-color'], style_tag)
+		set_css_attribute('color'			, base_styles[became]['color'			], style_tag)
+		set_css_attribute('border'			, base_styles[became]['border'			], style_tag)
+		set_css_attribute('font-style'		, base_styles[became]['font-style'		], style_tag)
+		set_css_attribute('font-weight'		, base_styles[became]['font-weight'		], style_tag)
+		set_css_attribute('text-decoration'	, base_styles[became]['text-decoration'	], style_tag)
 	}
 }
 
@@ -142,9 +143,9 @@ function encode_subclass_name (subclass_name) {
     chars.push(t[t.length-1])
  
     s1 = s2 = 0
-    for (i in chars) {
-        s1 += ch.indexOf(chars[i])*Math.pow(2, i)
-        s2 += ch.indexOf(chars[i])
+    for (i in chars) if (chars[i]) {
+		s1 += ch.indexOf(chars[i])*Math.pow(2, i)+1
+		s2 += ch.indexOf(chars[i])+1
 	}
  
 	t = (bin(s1).padEnd(13,'0') + bin(s2).padEnd(8,'0') + bin(cnt_of_vowels).padEnd(3,'0'))
@@ -154,36 +155,37 @@ function encode_subclass_name (subclass_name) {
 function encode_style (style_tag) {
 	let value = result = ''
 	value = get_css_attribute('background-color', style_tag)
-	result += compact_colour(value)
+	if (value == 'transparent') result += convert_10_to_sys(255)
+	else result += compact_colour(value)
 	value = get_css_attribute('color'           , style_tag)
-	result += compact_colour(value)
+	if (value == base_styles[MODE]['color']) result += convert_10_to_sys(255)
+	else result += compact_colour(value)
 	result += compact_colour(get_css_attribute('border', style_tag).split(' ')[2])
 
 	value = ''
-	value += get_css_attribute('font-style' , style_tag) == 'italic'? 'T' : 'F'
-	value += get_css_attribute('font-weight', style_tag) == 'bold'? 'T' : 'F'
+	value += get_css_attribute('font-style' , style_tag) == 'italic'? '1' : '0'
+	value += get_css_attribute('font-weight', style_tag) == 'bold'? '1' : '0'
 	switch(get_css_attribute('text-decoration', style_tag)) {
 		case '':
 		case 'none':
-			value += 'FF'
+			value += '00'
 			break
 
 		case 'line-through':
-			value += 'FT'
+			value += '01'
 			break
 
 		case 'underline':
-			value += 'TF'
+			value += '10'
 			break
 
 		case 'line-through underline':
 		case 'underline line-through':
-			value += 'TT'
+			value += '11'
 			break
 	}
-	value += (get_css_attribute('border', style_tag).split(' ')[0] == '1px'? 'T' : 'F')
-	value = convert_sys_to_10(value.replaceAll('T', '1').replaceAll('F', '0'), 2)
-	result += convert_10_to_sys(value)
+	value += (get_css_attribute('border', style_tag).split(' ')[0] == '1px'? '1' : '0')
+	result += convert_from_to(value, 2, 256) 
 
 	return result
 }
@@ -192,38 +194,44 @@ function save_settings () {
 	for (class_text in used_class_names) {
 		if (Object.keys(used_class_names[class_text]).length == 0) continue
 		let class_name = used_class_names[class_text][Object.keys(used_class_names[class_text])[0]].split('-')[0]
-		let encoded_str = ''
+		let encoded_str = []
+		let to_remove = []
 
 		for (subclass_text in used_class_names[class_text]) {
 			let subclass_name = used_class_names[class_text][subclass_text].split('-')[1]
 			let style_tag = document.getElementById('style_'+class_name+'-'+subclass_name)
 
 			if (!check_is_base(style_tag)) {
-				encoded_str += encode_subclass_name(subclass_name) + encode_style(style_tag)
+				encoded_str.push(encode_subclass_name(subclass_name) + encode_style(style_tag))
 			}
+			else to_remove.push(encode_subclass_name(subclass_name))
 		}
-		//console.log(class_name, encoded_str)
-		if (encoded_str != '') {
-			let loaded_cookie = readCookie(class_name)
-			if (loaded_cookie) { // if already exists
-				for (let i = 0; i < encoded_str.length; i += 7) { // for each encoded class
-					let index = loaded_cookie.indexOf(encoded_str.slice(i, i+3))
-					if (index !== -1) { // if setting already exists - replace
-						loaded_cookie = loaded_cookie.replace(loaded_cookie.slice(index+3, index+7), encoded_str.slice(i+3, i+7))
-					}
-					else { // if new setting
-						loaded_cookie += encoded_str.slice(i, i+7)
-					}
+		
+		let loaded_cookie = readCookie(class_name).split('|')
+		let result_cookie = [...encoded_str]
+
+		if (loaded_cookie[0] !== '') for (code of loaded_cookie) {
+			if (code.length != 7) code = try_debug(code)
+			if (to_remove.indexOf(code.slice(0, 3)) !== -1) continue
+			else {
+				let push_old = true
+				for (str of encoded_str) if (str.slice(0, 3) == code.slice(0, 3)) {
+					push_old = false
+					break
 				}
-				createCookie(class_name, loaded_cookie, 180)
+				if (push_old) result_cookie.push(code)
 			}
-			else createCookie(class_name, encoded_str, 180)
 		}
+
+		createCookie(class_name, result_cookie.join('|'), 180)
 	}
 }
 
+const special_colour_code = {255: 'dont_use', 254: 'transparent', 253: 'inherit'}
 function unpack_colour (symbol) {
-	symbol = convert_sys_to_10(symbol)-1
+	symbol = convert_sys_to_10(symbol)
+	if (symbol > 216) return special_colour_code[symbol]
+	symbol--
 	symbol = ('#' + convert_10_to_sys((Math.floor(symbol/36)%6)*51, 16).padEnd(2, '0') + 
 			        convert_10_to_sys((Math.floor(symbol/6 )%6)*51, 16).padEnd(2, '0') + 
 			        convert_10_to_sys((Math.floor(symbol/1 )%6)*51, 16).padEnd(2, '0'))
@@ -232,24 +240,34 @@ function unpack_colour (symbol) {
 
 function decode_style (str) {
 	let style_content = []
-	style_content.push('background-color: '+unpack_colour(str[0]))
-	style_content.push('color: '           +unpack_colour(str[1]))
+	let value = ''
 
-	let value = convert_10_to_sys(convert_sys_to_10(str[3]), 2).padStart(5, '0')
+	value = unpack_colour(str[0])
+	if (value != 'dont_use') style_content.push('background-color: '+unpack_colour(str[0]))
+	value = unpack_colour(str[1])
+	if (value != 'dont_use') style_content.push('color: '           +unpack_colour(str[1]))
 
-	style_content.push(`border: ${value[4] == '1'? '1' : '0'}px solid ${unpack_colour(str[2])}`)
+	value = convert_10_to_sys(convert_sys_to_10(str[3]), 2).padStart(5, '0')
 
-	style_content.push('font-style: '      + (value[0] == '1'? 'italic'      : 'normal'))
-	style_content.push('font-weight: '     + (value[1] == '1'? 'bold'        : 'normal')) 
+	if (value[4] == '1') style_content.push(`border: 1px solid ${unpack_colour(str[2])}`)
+
+	if (value[0] == '1') style_content.push('font-style: italic')
+	if (value[1] == '1') style_content.push('font-weight: bold')
 	style_content.push('text-decoration: ' + (value[2] == '1'? 'underline '  : ''      )
 					       + (value[3] == '1'? 'line-through': ''      ))
 
 	if (style_content[style_content.length-1] == 'text-decoration: ') {
-		style_content[style_content.length-1] += 'none'
+		style_content.pop()
 	}
 
 	//console.log(`{ ${style_content.join('; ')}; }`)
 	return `{ ${style_content.join('; ')}; }`
+}
+
+function try_debug (code) {
+	if (code.length == 3) return code + encode_style(base_style_classes[MODE])
+	else if (code.length == 6) return code + '0'
+	else return '___0000'
 }
 
 function load_settings () {
@@ -268,17 +286,51 @@ function load_settings () {
 			}
 
 			let decoded_styles = {}
-			for (key in encoded_subclass_names) {
-				let index = loaded_cookie.indexOf(key)
-				if (index !== -1) {
-					decoded_styles[encoded_subclass_names[key]] = decode_style(loaded_cookie.slice(index+3, index+7))
+			for (code of Object.values(loaded_cookie.split('|'))) {
+				if (code.length != 7) code = try_debug(code)
+				for (key in encoded_subclass_names) if (code.slice(0, 3) == key) {
+					decoded_styles[encoded_subclass_names[key]] = decode_style(code.slice(3, 7))
+					break
 				}
 			}
 
-			for (subclass_name in decoded_styles) {
+			for (subclass_text in used_class_names[class_text]) {
+				let subclass_name = used_class_names[class_text][subclass_text].split('-')[1]
+				if (decoded_styles[subclass_name]) {
+					old_style_tag = document.getElementById('style_'+class_name+'-'+subclass_name)
+					if (old_style_tag) head.removeChild(old_style_tag)
+					create_class(class_name+'-'+subclass_name, decoded_styles[subclass_name])
+				}
+				else {
+					old_style_tag = document.getElementById('style_'+class_name+'-'+subclass_name)
+					if (old_style_tag) old_style_tag.innerHTML  = `.${class_name+'-'+subclass_name} { }`
+				}
+			}
+		}
+		else {
+			for (subclass_text in used_class_names[class_text]) {
+				let subclass_name = used_class_names[class_text][subclass_text].split('-')[1]
 				old_style_tag = document.getElementById('style_'+class_name+'-'+subclass_name)
-				if (old_style_tag) head.removeChild(old_style_tag)
-				create_class(class_name+'-'+subclass_name, decoded_styles[subclass_name])
+				if (old_style_tag) old_style_tag.innerHTML  = `.${class_name+'-'+subclass_name} { }`
+			}
+		}
+	}
+}
+
+
+function set_default_styles () {
+	for (class_text in used_class_names) {
+		for (subclass_text in used_class_names[class_text]) {
+			let class_name = used_class_names[class_text][subclass_text]
+			style_tag = document.getElementById('style_'+class_name)
+			if (style_tag) style_tag.innerHTML  = `.${class_name} { }`
+		}
+	}
+
+	for (cl_name in base_text_styles) {
+		for (scl_name in base_text_styles[cl_name]) {
+			for (attrib in base_text_styles[cl_name][scl_name]) {
+				set_css_attribute(attrib, base_text_styles[cl_name][scl_name][attrib], cl_name+'-'+scl_name)
 			}
 		}
 	}
