26acc98b1b576e54fc1a8f71cfe9d8c6e3084e2b
KGlebB <nikiforov02@list.ru>
2022-02-19 18:57:36 +0300

    Buttons updated

--- js/html_editor/main_page.js
+++ js/html_editor/main_page.js
@@ -47,9 +47,11 @@ function generate_rasp_page(group)
             <h2 class="group_name" id="Group_Name">${group.prefix[0]=="преподаватель" ? "Преподаватель" : "Группа"} ${group.name.replace("_", " ")}</h2>
             <div class="header-main-right">
                 <div class="switchers">
-                    <button onclick="change_tracking_status()">время</button>
+                    <div class="timetrack-switcher" onclick="change_tracking_status();">
+                        <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 20 20" height="36px" viewBox="0 0 20 20" width="36px"></svg>
+                    </div>
                     <div class="editbar-switcher" onclick="editbarOpen();">
-                        <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 20 20" height="36px" viewBox="0 0 20 20" width="36px" fill="#000000"></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 20 20" height="36px" viewBox="0 0 20 20" width="36px"></svg>
                     </div>
                     <div class="mode-switcher" onclick="ChangeTheme();">
                         <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 20 20" height="36px" viewBox="0 0 20 20" width="36px"></svg>
@@ -70,15 +72,15 @@ function generate_rasp_page(group)
         <aside class="editbar editbar--hidden" id="Editbar">
             <p class="editbar-title">Настройки отображения пар</p>
             <a class="editbar-close" onclick="editbarClose();">
-                <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px"></svg>
             </a>
             <div class="editbar-main">
                 <div class="editbar-main-choice" id="EditOrder">
                 </div>
                 <div class="editbar-main-menu">
                     <a class="editbar-main-menu__button editbar-main-menu__button--accept" onclick="save_settings();">Применить</a>
-                    <a class="editbar-main-menu__button editbar-main-menu__button--import" style="color: #CCCCCC">Импортировать</a>
-                    <a class="editbar-main-menu__button editbar-main-menu__button--export" style="color: #CCCCCC">Экспортировать</a>
+                    <a class="editbar-main-menu__button editbar-main-menu__button--disabled editbar-main-menu__button--import" disabled>Импортировать</a>
+                    <a class="editbar-main-menu__button editbar-main-menu__button--disabled editbar-main-menu__button--export" disabled>Экспортировать</a>
                     <a class="editbar-main-menu__button editbar-main-menu__button--cancel" onclick="load_settings();">Отменить</a>
                     <a class="editbar-main-menu__button editbar-main-menu__button--default" onclick="set_default_styles()">По умолчанию</a>
                 </div>
@@ -86,7 +88,7 @@ function generate_rasp_page(group)
         </aside>
         <aside class="filterbar filterbar--hidden" id="Filterbar">
         <a class="filterbar-close" onclick="filterbarClose();">
-            <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px"></svg>
         </a>
             <p class="filterbar-title">Пользовательские фильтры</p>
             <div class="filterbar-main">
