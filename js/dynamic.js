48b6f96d10af5386da935903c0922a252382febb
Maxim Makarov <74417675+WhiteRain7@users.noreply.github.com>
2022-03-11 20:39:24 +0300

    unification of "hidden" and new functions

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,33 +1,29 @@
 function Switch(id)
 {
-    document.getElementById(`${id}`).classList.toggle(`${id.toLowerCase()}--hidden`);
+    document.getElementById(id).classList.toggle('hidden');
     return false;
 }
 
 function switch_theme_list() 
 {
-    let theme_list = document.getElementById("theme_list")
-    if (theme_list.classList.contains("clr_theme_list--hidden")) {
-        theme_list.classList.remove("clr_theme_list--hidden");
-        //genFilterList(listName);
-    }
-    else theme_list.classList.add("clr_theme_list--hidden");
+    document.getElementById("theme_list").classList.toggle("hidden");
     return false;
 }
 
 function open_theme_editor () {
-    document.getElementById("theme_editor").classList.remove("theme_aside_editor--hidden");
+    document.getElementById("theme_editor").classList.remove("hidden");
     fill_theme_editor()
     return false;
 }
 function close_theme_editor () {
-    document.getElementById("theme_editor").classList.add("theme_aside_editor--hidden");
+    document.getElementById("theme_editor").classList.add("hidden");
     return false
 }
 
 function editbarOpen()
 {
-    document.getElementById("Editbar").classList.remove("editbar--hidden");
+    document.getElementById("Editbar").classList.remove("hidden");
+    Switch('setting_menu');
     genEditOrder();
     return false;
 }
@@ -40,26 +36,33 @@ function editbarClose()
             there_are_changes = false
         }
     }
-    document.getElementById("Editbar").classList.add("editbar--hidden");
-    document.getElementById("Filterbar").classList.add("filterbar--hidden");
+    document.getElementById("Editbar").classList.add("hidden");
+    document.getElementById("Filterbar").classList.add("hidden");
     return false;
 }
 
 function filterbarShow(listName)
 {
-    document.getElementById("Filterbar").classList.remove("filterbar--hidden");
+    document.getElementById("Filterbar").classList.remove("hidden");
     genFilterList(listName);
     return false;
 }
 
 function filterbarClose()
 {
-    document.getElementById("Filterbar").classList.add("filterbar--hidden");
+    document.getElementById("Filterbar").classList.add("hidden");
     return false;
 }
 
+function switch_issue_report () {
+    if (document.getElementById("issue_form").classList.contains("hidden")) {
+        open_issue_report()
+    }
+    else close_issue_report()
+}
+
 function open_issue_report () {
-    document.getElementById("issue_form").classList.remove("aside_form--hidden");
+    document.getElementById("issue_form").classList.remove("hidden");
     fid = 'start';
     AF_selected = '';
     AF_value = '';
@@ -69,6 +72,14 @@ function open_issue_report () {
 }
 
 function close_issue_report () {
-    document.getElementById("issue_form").classList.add("aside_form--hidden");
+    document.getElementById("issue_form").classList.add("hidden");
     document.getElementById("issue_form").innerHTML = '';
+}
+
+auto_alt_click = false
+function switch_alt_click () {
+    auto_alt_click = !auto_alt_click
+    if (auto_alt_click) document.getElementsByClassName('enable_alt_click')[0].setAttribute('style', 'background: var(--color-light)')
+    else document.getElementsByClassName('enable_alt_click')[0].removeAttribute('style')
+    return false
 }
