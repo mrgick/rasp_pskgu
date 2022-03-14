8e52687e20b63008de77d28c0521526492d059e7
Maxim Makarov <74417675+WhiteRain7@users.noreply.github.com>
2022-03-14 19:46:29 +0300

    added check for relevance

--- js/time.js
+++ js/time.js
@@ -29,6 +29,11 @@ const weekNames = [
     "Среда", "Четверг", "Пятница", "Суббота"
 ];
 
+const was_updated = {   'year' : ['год'   , 'года'  , 'лет'    ],
+                        'month': ['месяц' , 'месяца', 'месяцев'],
+                        'week' : ['неделю', 'недели', 'недель' ],
+                        'day'  : ['день'  , 'дня'   , 'дней'   ]}
+
 function getYearWeek(now)
 {
     let onejan = new Date(now.getFullYear(), 0, 1);
@@ -40,4 +45,38 @@ function getCurrentWeek(first_date)
     let mondate = getYearWeek(new Date(get_monday(first_date)));
     let todate = getYearWeek(new Date(get_monday(new Date())));
     return todate - mondate + 1;
+}
+
+function try_insert_ago (period, interval, diference) {
+    let result = Math.floor(diference/interval)
+    if (result > 0) return `${result} ${was_updated[period][result == 1? 0 : (result < 5? 1 : 2)]} назад)`
+    else return false
+}
+
+function insert_date_of_last_update (last_date, last_update) {
+    let difference = new Date() - last_update
+    difference /= 1000*60*60*24
+    
+    let div = document.getElementById('date_of_last_update')
+    if (div) {
+        div.innerHTML = `
+        <p>Последнее обновление:</p>
+        <p>${last_update.getDate()} ${monthNames[last_update.getMonth()]} ${last_update.getFullYear()} (</p>
+        `
+
+        if (difference < 1) div.innerHTML += '<p>Обновлено сегодня</p>'
+        else if (difference < 2) div.innerHTML += '<p>Обновлено вчера</p>'
+        else for (i of [['year', 365], ['month', 30], ['week', 7], ['day', 1]]) {
+            let result = try_insert_ago(...i, difference)
+            if (result) {
+                div.children[1].innerText += result
+                break
+            }
+        }
+    }
+
+    if (difference > 30) {
+        if (div) div.children[1].setAttribute('style', 'color: var(--color-error)')
+        if (last_date <= new Date()) document.getElementById("aside_warning").classList.remove("hidden")
+    }  
 }
