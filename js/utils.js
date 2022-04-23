edcfa20b7a93633d57ea52298de571b1559e2ac1
Maxim Makarov <74417675+WhiteRain7@users.noreply.github.com>
2022-04-23 19:37:00 +0300

    new utilities

--- js/utils.js
+++ js/utils.js
@@ -145,4 +145,20 @@ function decode_win1251(s){
     s.replace(/%/g, ",0x").slice(1).split(",")
     )))
     return s
+function componentToHex (c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
 }
+  
+function rgb_to_hex (r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function hex_to_rgb (hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    } : null;
+  }
