a00d45b2ef02a8bf3dbaaedf38cf22e9e70584df
Maxim Makarov <74417675+WhiteRain7@users.noreply.github.com>
2022-04-26 19:18:38 +0300

    Update events.js

--- js/html_editor/user settings/events.js
+++ js/html_editor/user settings/events.js
@@ -307,20 +307,20 @@ let invitation_preset = base_invitation_preset
 
 function load_invitation_preset () {
     let loaded = readCookie('event_invitation')
-    if (loaded) invitation_preset = loaded.replaceAll('$n', '\n')
+    if (loaded) invitation_preset = loaded
     document.getElementById('invitation_editor').children[1].value = invitation_preset
 }
 
 function save_new_invitation (block) {
     block.classList.add('hidden')
     invitation_preset = document.getElementById('invitation_editor').children[1].value
-    createCookie('event_invitation', invitation_preset.replaceAll('\n', '$n'), 180)
+    createCookie('event_invitation', invitation_preset, 180)
 }
 function set_base_invitation (block) {
     block.classList.add('hidden')
     invitation_preset = base_invitation_preset
     document.getElementById('invitation_editor').children[1].value = invitation_preset
-    createCookie('event_invitation', invitation_preset.replaceAll('\n', '$n'), 180)
+    createCookie('event_invitation', invitation_preset, 180)
 }
 function invitation_changed () {
     document.getElementById('invitation_editor').children[4].classList.remove('hidden')
@@ -406,7 +406,7 @@ function make_invitation (usr_event) {
                 break
         }
     }
-    return invitation_str
+    return invitation_str.replace('$n', '\n')
 }
 
 const event_hash_convertations = [
