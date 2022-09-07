63bab2fc21006216de0db579e2fbe19af0013dfc
Maxim Makarov <74417675+WhiteRain7@users.noreply.github.com>
2022-09-07 22:54:01 +0300

    ignore this

--- js/html_editor/main_page.js
+++ js/html_editor/main_page.js
@@ -49,7 +49,7 @@ function generate_main_page()
             <input class="search-form-input" name="find_group_name" type="search" autocomplete="off" placeholder="группа/преподаватель" />
             <button class="search-form-button" onclick="adapt_search_text()" type="submit">Поиск</button>
         </form>
-        <a class="search-showall" href="?list">Показать всё</a>
+        <!--a class="search-showall" href="?list">Показать всё</a-->
     </div>
     `
     add_footer(to = document.getElementById('MAIN'), as_block = false)
